da2794372f921dbf7b966005be7d82b524d40e80
Ritik Raj <ritikraj2699@outlook.com>
2023-06-28 01:00:44 +0530

    added signin

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from .models import Profile
 
@@ -41,4 +41,17 @@ def signup(request):
         return render(request, 'signup.html')
 
 def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid Credentials')
+            return redirect('signin')
+
     return render(request,'signin.html')
