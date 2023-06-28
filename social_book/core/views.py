661daf805e88c0e4be8d9ab5e60068a4aad9c5dd
Ritik Raj <ritikraj2699@outlook.com>
2023-06-28 22:17:13 +0530

    added update profile

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -15,11 +15,27 @@ def index(request):
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
 
-    # if request.method == 'POST':
-    #     if request.FILES.get('image') == None:
-    #         image = user_profile.profileimg
-    #         bio = user_profile.POST['bio']
-    #         location = user_profile.POST['location']
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect(request,'settings')
     return render(request, 'setting.html', {'user_profile': user_profile})
 
 
